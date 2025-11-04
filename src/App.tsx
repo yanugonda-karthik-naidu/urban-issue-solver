9053c8debcd8b932b4748d1e9cf81614de0b1d5b
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-11-04 11:54:17 +0530

    updated Community

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
-
-
-// User Page
 import { FloatingChatbot } from "./components/FloatingChatbot";
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
